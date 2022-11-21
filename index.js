3d4b17cf7d27f1998e46db5bb8ec03079fe2f48a
sr-sahoo <techno@soumyaranjan.com>
2022-11-21 18:02:26 +0530

    updated1.0

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+import express from 'express'
+import pkg from 'pg'
+import cors from 'cors'
+
+const { Client } = pkg
+const client = new Client({
+    connectionString: 'postgres://mbenjfew:hRq7_SRn90Um0GVDn4poRR8QtqVpjnMT@mouse.db.elephantsql.com/mbenjfew',
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
-app.listen(process.env.PORT || 3000)
+
+client.connect()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+app.listen(2000, console.log('App is running on 2000'))
+
+
+
+const getRegisteredList = async(req, res) => {
+    try {
+        const list = await client.query('select * from register')
+        res.status(200).send({
+            message: 'Sucess',
+            data: list.rows
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(200).send({
+            message: 'Error while getting list'
+        })
+    }
+}
+
+app.get('/register', getRegisteredList)
